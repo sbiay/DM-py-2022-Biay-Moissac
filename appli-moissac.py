99c65714a4784eb4d065966a8cc6a9042b7afbc5
Sebastien Biay <sbiayha@outlook.fr>
2021-11-27 08:28:05 +0100

    Création d'une page d'accueil html ; échec d'association d'un CSS local

---
+++ appli-moissac.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask("Application")
+
+
+@app.route("/")
+def accueil():
+    return render_template("accueil.html", nom="Scriptorium de Moissac")
+
+
+@app.route("/codices/<int:cod_id>")
+def notice_codex(cod_id):
+    return "Notice " + str(cod_id) + " du catalogue de Jean Dufour"
+
+
+app.run()
