45b9ff490164b76333b1651ad42f8c904a9e2c09
Sebastien Biay <sbiayha@outlook.fr>
2021-12-07 12:38:02 +0100

    MAJ

--- dev_test.py
+++ dev_test.py
@@ -1,13 +1,14 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy  # On peut trouver flask.ext.sqlalchemy
 
-app = Flask("lib-moissac")
+app = Flask("Nom")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/libMoissac.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# .config est un dict
 db = SQLAlchemy(app)
 
-query = db.engine.execute('SELECT * FROM codices')
+query = db.engine.execute("SELECT * FROM codices")
 print(query)
+# Vous pouvez remplacer fetchmany par fetchall ou fetchone en supprimant le 2
 for x in query.fetchall():
-    for donnes in x:
-        print(donnes)
+    print(x)
