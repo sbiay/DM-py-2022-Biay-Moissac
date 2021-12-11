6f5c8cab749cb4668a3893b5a594d09657943789
Sebastien Biay <sbiayha@outlook.fr>
2021-12-11 16:26:49 +0100

    Dev

--- db/libMoissac_complete.sql
+++ db/libMoissac_complete.sql
@@ -1,82 +1,75 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS codices (
-    id INTEGER PRIMARY KEY,     --? Identifiant unique de chaque codex dans la base
-    cote TEXT NOT NULL,         --? Cote du codex dans la collection où il est conservé
-    id_technique TEXT,          --? Identifiant de type ark, ou autre
-    reliure_descript TEXT,      --? Description de la reliure du codex
-    histoire TEXT               --? Description de l'histoire du codex et de sa conservation
+CREATE TABLE IF NOT EXISTS "codices" (
+	"id"	INTEGER,
+	"cote"	TEXT NOT NULL,
+	"id_technique"	TEXT,
+	"reliure_descript"	TEXT,
+	"histoire"	TEXT,
+	PRIMARY KEY("id")
 );
-
-CREATE TABLE IF NOT EXISTS unites_codico (
-    id INTEGER PRIMARY KEY,     --? Identifiant unique de l'unité codicologique (UC) dans la base
-    descript TEXT,              --? Description physique de l'unité codicologique et des mains
-    loc_init INTEGER DEFAULT NULL,  --? Indique le début de la foliotation de l'unité courante dans le codex
-    loc_init_v BOOLEAN DEFAULT NULL, --? Est vrai si l'unité codicologique commence sur un verso
-    loc_fin INTEGER DEFAULT NULL,     --? Indique la fin de la foliotation de l'unité courante dans le codex
-    loc_fin_v BOOLEAN DEFAULT NULL,  --? Est vrai si l'unité codicologique commence sur un verso
-    date_pas_avant INTEGER NOT NULL, --? Borne chronologique de début pour la fabrication de l'UC
-    date_pas_apres INTEGER NOT NULL, --? Borne chronologique de fin pour la fabrication de l'UC
-    code_id INTEGER REFERENCES codices(id) --? Identifiant du codex auquel l'unité codico. appartient
+CREATE TABLE IF NOT EXISTS "unites_codico" (
+	"id"	INTEGER,
+	"descript"	TEXT,
+	"loc_init"	INTEGER DEFAULT NULL,
+	"loc_init_v"	BOOLEAN DEFAULT NULL,
+	"loc_fin"	INTEGER DEFAULT NULL,
+	"loc_fin_v"	BOOLEAN DEFAULT NULL,
+	"date_pas_avant"	INTEGER NOT NULL,
+	"date_pas_apres"	INTEGER NOT NULL,
+	"code_id"	INTEGER,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("code_id") REFERENCES "codices"("id")
 );
-
-CREATE TABLE IF NOT EXISTS oeuvres (
-    id INTEGER PRIMARY KEY,     --? Identifiant unique d'une oeuvre littéraire dans la base
-    titre TEXT NOT NULL,        --? Titre de l'oeuvre dans sa forme d'autorité selon Data-BNF
-    data_bnf INTEGER,           --? Identifiant Data-BNF de l'oeuvre
-    partie_de REFERENCES oeuvres(id), --? Lien entre une partie d'oeuvre et l'oeuvre complète
-    auteur REFERENCES personne(id) --? Lien entre l'oeuvre et son auteur
+CREATE TABLE IF NOT EXISTS "oeuvres" (
+	"id"	INTEGER,
+	"titre"	TEXT NOT NULL,
+	"data_bnf"	INTEGER,
+	"partie_de"	,
+	"auteur"	,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("partie_de") REFERENCES "oeuvres"("id"),
+	FOREIGN KEY("auteur") REFERENCES "personne"("id")
 );
-
-CREATE TABLE IF NOT EXISTS contient (
-    oeuvre REFERENCES oeuvres(id),
-    unites_codico REFERENCES unites_codico(id)
+CREATE TABLE IF NOT EXISTS "contient" (
+	"oeuvre"	,
+	"unites_codico"	,
+	FOREIGN KEY("oeuvre") REFERENCES "oeuvres"("id"),
+	FOREIGN KEY("unites_codico") REFERENCES "unites_codico"("id")
 );
-
-CREATE TABLE IF NOT EXISTS personne (
-    id INTEGER PRIMARY KEY,     --? Identifiant unique d'une personne physique dans la base
-    nom TEXT,                   --? Nom de la personne dans sa forme d'autorité selon Data-BNF
-    data_bnf INTEGER           --? Identifiant Data-BNF de la personne
+CREATE TABLE IF NOT EXISTS "personne" (
+	"id"	INTEGER,
+	"nom"	TEXT,
+	"data_bnf"	INTEGER,
+	PRIMARY KEY("id")
 );
 INSERT INTO "codices" ("id","cote","id_technique","reliure_descript","histoire") VALUES (1,'Latin 2989','ark:/12148/cc60815j','Reliure du XVIIIe siècle sur ais de carton en maroquin rouge aux armes royales ; dos décoré par des poinçons, au chiffre royal ; titre au dos en lettres dorées : « Cassianus de monachis ». Contregardes et gardes en parchemin.','Produit dans le Sud-Ouest de la France, le ms. a fait partie de la bibliothèque de l’abbaye de Saint-Pierre de Moissac : il est recensé dans le catalogue de 1678 parmi les livres que Colbert fit venir de Moissac à Paris, mais il est absent du catalogue de Moissac rédigé peu avant cette date (cf. Dufour, « La composition », p. 210). Le volume est donc entré dans la collection de Jean-Baptiste Colbert (1619-1683) (cf. cote ancienne au f. 3r, « Codex Colber. 6156 ») et il entra dans la Bibliothèque du roi en 1732, avec d’autres manuscrits colbertins.'),
  (2,'Latin 2077','ark:/12148/cc599714','Reliure maroquin rouge aux armes de Colbert.','Provient de l''abbaye de Moissac, cf. Delisle, Cab. des mss ., I, 519.');
-INSERT INTO "unites_codico" (
-"id",
-"descript",
-"loc_init",
-"loc_init_v",
-"loc_fin",
-"loc_fin_v",
-"date_pas_avant",
-"date_pas_apres",
-"code_id"
-)
-VALUES
-       (1,
-        'Parchemin. 154 ff., précédés et suivis d’une garde en parchemin. 180 x 123 mm (justif. 130 x 80 mm). Écriture minuscule caroline d’une main principale.',
-        NULL,
-        NULL,
-        NULL,
-        NULL,
-        975,
-        1000,
-        1)
-       ;
-INSERT INTO "oeuvres" ("id","titre","data_bnf","partie_de","auteur") VALUES
- (1,'Institutions cénobitiques',13771861,NULL,1),
+INSERT INTO "unites_codico" ("id","descript","loc_init","loc_init_v","loc_fin","loc_fin_v","date_pas_avant","date_pas_apres","code_id") VALUES (1,'Parchemin. 154 ff., précédés et suivis d’une garde en parchemin. 180 x 123 mm (justif. 130 x 80 mm). Écriture minuscule caroline d’une main principale.',NULL,NULL,NULL,NULL,975,1000,1),
+ (2,NULL,1,1,120,1,981,1020,2),
+ (3,'Ecriture régulière de la première moitié du XIe siècle (Dufour, BSM)',121,0,173,0,1001,1050,2),
+ (4,NULL,174,0,175,0,1100,1120,2),
+ (5,NULL,122,0,123,0,1131,1170,2);
+INSERT INTO "oeuvres" ("id","titre","data_bnf","partie_de","auteur") VALUES (1,'Institutions cénobitiques',13771861,NULL,1),
  (2,'De officiis ministrorum',NULL,NULL,2),
  (3,'De conflictu vitiorum et virtutum',16746816,NULL,3),
  (4,'Conlatio cum Maximino Arrianorum episcopo',12076565,NULL,4),
  (5,'Contra Maximinum haereticum Arianorum episcopum',12076556,NULL,4),
- (6,'Quatre-vingt trois questions différentes',13620219,NULL,4);
+ (6,'Quatre-vingt trois questions différentes',13620219,NULL,4),
+ (7,'De fide et symbolo',15127117,NULL,4),
+ (8,'De Genesi contra Manichaeos',12489317,NULL,4),
+ (9,'Sermones',11983905,NULL,4);
+INSERT INTO "contient" ("oeuvre","unites_codico") VALUES (1,1),
+ (2,3),
+ (3,3),
+ (4,2),
+ (5,2),
+ (6,2),
+ (7,2),
+ (8,2),
+ (9,3),
+ (6,3);
 INSERT INTO "personne" ("id","nom","data_bnf") VALUES (1,'Jean Cassien (saint, 0360?-0432?)',12044269),
  (2,'Ambroise (saint, 0340?-0397)',11888642),
  (3,'Ambroise Autpert (730?-784)',16708997),
  (4,'Augustin (saint, 0354-0430)',11889551);
-INSERT INTO "contient" ("oeuvre","unites_codico") VALUES
- (1,1),
- (2,2),
- (3,2),
- (4,2),
- (5,2),
- (6,2);
 COMMIT;
--- dev_test.py
+++ dev_test.py
@@ -3,113 +3,6 @@ import csv
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-# Dictionnaire de travail
-prem_codices = [
-    {
-        'cote': "Paris, BNF, Latin 2077",
-        'format': '''Initiales en couleur, grandes initiales peintes à entrelacs (32v, 38, 45v, 59v, 96, 113,
-        119).
-        Rubriques en capitale rustique.
-        Croquis dans les marges inférieures (55v, 66, 87v, 98v, 106, 108) et 10 dessins (162v-173).
-        — Les ff. 122 et 123 ont été intercalés.
-        Les ff. 123v et 161 sont blancs.
-        Parchemin.179 ff.350 × 230 mm.
-        Reliure maroquin rouge aux armes de Colbert.''',
-        'partLoc': '',
-        'date': 'Fin du IXe siècle',
-        'contenu': [
-            {
-                'auteur': 'S. Augustinus',
-                'titre': ''
-            },
-            {
-                'auteur': 'Paschasius Radbertus',
-                'titre': ''
-            },
-            {
-                'auteur': 'S. Leodegarius',
-                'titre': ''
-            },
-            {
-                'auteur': 'Halitgarius Cameracensis',
-                'titre': ''
-            },
-            {
-                'auteur': 'Ambrosius Autpertus',
-                'titre': ''
-            },
-        ]
-    },
-    {
-        'cote': "Paris, BNF, Latin 2989",
-        'format': '''Écriture minuscule caroline d’une main principale.
-        Initiales décorées à entrelacs, motifs végétaux et palmettes, tracées à l’encre brune et colorées en rose, vert,
-        jaune et mauve : f. 3v Veteris (9 lignes) ; f. 6v De (6 lignes) ; f. 12v Duplici (7 lignes) ;
-        f. 24r De (7 lignes) ; f. 33r De (7 lignes) ; f. 56r Quintus (7 lignes) ; f. 78v Secundum (6 lignes) ;
-        f. 87v Tercius (11 lignes) ; f. 101r Quarto (6 lignes) ; f. 111v Quintum (5 lignes) ;
-        f. 115r Sextum (5 lignes) ; f. 129r Septimum (8 lignes) ; f. 136v Octavum (7 lignes) ;
-        f. 154v Propagatori (7 lignes).
-        Petites initiales anthropomorphes et zoomorphes : f. 31v Cuius (3 lignes) ; f. 34v Quam (3 lignes) ;
-        f. 47v Cuius (3 lignes) ; f. 83v Haec (4 lignes).
-        Initiales de couleur à l’encre rouge, verte et jaune ; capitales rehaussées de rouge, rose, vert et jaune.
-        Premiers mots de chaque chapitre en lettres mixtes de capitales et minuscules, rehaussées de rouge.
-        Rubriques en capitales rustiques à l’encre rouge, numéros de chapitre à l’encre rouge.''',
-        'partLoc': '',
-        'date': 'Xe siècle (dernier quart)',
-        'contenu': [
-            {
-                'auteur': 'Iohannes Cassianus',
-                'titre': 'De institutis coenobiorum'
-            },
-        ]
-    }
-]
-
-
-def iiif(ark):
-    """A partir d'un identifiant ark, cette fonction donne comme valeur de retour un objet Json
-    contenant les métadonnées d'un objet hébergé sur Gallica :
-
-    :param ark: identifiant ark
-    :type ark: str
-    :returns: objet Json contenant les métadonnées du manuscrit
-    :type return: dict
-    """
-    
-    # Requête HTML sur Gallica
-    r = requests.get(f"http://gallica.bnf.fr/iiif/{ark}/manifest.json")
-    jsonf = r.json()
-    # Affichage des métadonnées de l'objet
-    for meta in jsonf["metadata"]:
-        print("{label} : {value}".format(label=meta["label"], value=meta["value"]))
-    
-    return jsonf
-
-
-def concordance(cod_id):
-    """A partir d'un identifiant numérique, cette fonction interroge la concordance entre le catalogue
-    de Dufour et une liste d'identifiants ark de la BNF et retourne l'objet Json correspondant
-    via la fonction iiif"""
-    with open("csv/dufour_ark.csv") as f:
-        cont_csv = csv.DictReader(f)
-        
-        # Ecriture d'une liste dont chaque item est un dictionnaire ayant :
-        # - Pour clés les intitulés "iduf" et "ark"
-        # - Pour valeurs les valeurs correspondantes
-        concord_duf_ark = []
-        for row in cont_csv:
-            concord_duf_ark.append(row)
-        
-        for enreg in concord_duf_ark:
-            if str(cod_id) == enreg["iduf"]:
-                json_codex = iiif(enreg["ark"])  # json_codex est le dictionnaire contenant les métadonnées du
-                # manuscrit cherché par son identifiant
-    return json_codex
-
-
-
-
-
 
 # Définition de mon application
 app = Flask("lib-moissac")
@@ -117,7 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/libMoissac.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 # Définition de mes classes d'objets
+# ATTENTION, il faudra veiller à bien appliquer le modèle logique
 class Codices(db.Model):
     id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
     cote = db.Column(db.String)
@@ -125,36 +20,60 @@ class Codices(db.Model):
     reliure_descript = db.Column(db.Text)
     histoire = db.Column(db.Text)
 
+
+class Unites_codico(db.Model):
+    id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
+    # Description physique
+    descript = db.Column(db.Text)
+    # Localisation d'une unité dans un codex (f. n-f. m)
+    loc_init = db.Column(db.Integer, default=None)
+    loc_init_v = db.Column(db.Boolean, default=None)
+    loc_fin = db.Column(db.Integer, default=None)
+    loc_fin_v = db.Column(db.Boolean, default=None)
+    date_pas_avant = db.Column(db.Integer, nullable=False)
+    date_pas_apres = db.Column(db.Integer, nullable=False)
+    code_id = db.Column(db.Integer, nullable=False)
+
+
 class Oeuvres(db.Model):
     id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
     titre = db.Column(db.Text)
     data_bnf = db.Column(db.Integer)
     partie_de = db.Column(db.Boolean)
     auteur = db.Column(db.Integer)
+
+
+
+def notice_codex(num):
+    codex = Codices.query.get(num)
     
-@app.route("/")
-def conteneur():
-    return render_template("conteneur.html", nom="Bibliothèque de Moissac")
-
-
-@app.route("/pages/")
-def accueil():
-    return render_template("pages/accueil.html", nom="Bibliothèque de Moissac")
-
-
-@app.route("/pages/codices/<int:cod_id>")
-def notice_codex(cod_id):
-    codices = Codices.query.all()
-    print("yes")
-    codex = Codices.query.get(cod_id)
+    # Requête portant sur les unités codicologiques enfants d'un codex désigné par son identifiant
+    listUC_enfants = Unites_codico.query.filter(Unites_codico.code_id == num).all()
+    for UC in listUC_enfants:
+        description = UC.descript
+        print(description)
+        # Conditions portant sur le booléen relatif aux recto/verso au début et à la fin de l'UC
+        if UC.loc_init_v:
+            rvdebut = "v"
+        else:
+            rvdebut = ""
+        if UC.loc_fin_v:
+            rvfin = "v"
+        else:
+            rvfin = ""
+        localisation = f"f. {str(UC.loc_init)}{rvdebut}-{str(UC.loc_fin)}{rvfin}"
     
-    return render_template("pages/codices.html",
-                               titre=codex.cote,
-                               reliure=codex.reliure_descript,
-                               histoire=codex.histoire)
-
+    # REPRENDRE ICI : assigner les paramètres de date
+    
     # Test d'existence d'un index dans la liste des prem_codices :
-    #return render_template("pages/codices.html", message_erreur="Cette adresse ne correspond à aucune notice !")    
-    
+    codices = Codices.query.all()
+    if num <= len(codices):
+        # Si l'id passé dans l'URL n'est pas plus grand que la liste
+        # de tous les codices, alors :
+        print(codex.cote)
+        print(codex.reliure_descript)
+    else:
+        print("Cette adresse ne correspond à aucune notice !")
 
-notice_codex(1)
+
+notice_codex(3)
