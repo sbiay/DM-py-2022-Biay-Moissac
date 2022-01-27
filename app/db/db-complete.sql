7b3c5a115773a4aa2d36160d1f546b3fc1822b0e
Sebastien Biay <sbiayha@outlook.fr>
2022-01-27 20:44:50 +0100

    Suppression de production

--- app/db/db-complete.sql
+++ app/db/db-complete.sql
@@ -1,14 +1,5 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "codices" (
-	"id"	INTEGER,
-	"cote"	TEXT NOT NULL,
-	"id_technique"	TEXT,
-	"descript_mater"	TEXT,
-	"histoire"	TEXT,
-	"lieu_conservation"	INTEGER,
-	FOREIGN KEY("lieu_conservation") REFERENCES "lieux"("id"),
-	PRIMARY KEY("id")
-);
+DROP TABLE IF EXISTS "unites_codico";
 CREATE TABLE IF NOT EXISTS "unites_codico" (
 	"id"	INTEGER,
 	"descript"	TEXT,
@@ -22,6 +13,7 @@ CREATE TABLE IF NOT EXISTS "unites_codico" (
 	FOREIGN KEY("code_id") REFERENCES "codices"("id"),
 	PRIMARY KEY("id")
 );
+DROP TABLE IF EXISTS "oeuvres";
 CREATE TABLE IF NOT EXISTS "oeuvres" (
 	"id"	INTEGER,
 	"titre"	TEXT NOT NULL,
@@ -29,37 +21,26 @@ CREATE TABLE IF NOT EXISTS "oeuvres" (
 	"partie_de"	,
 	"auteur"	,
 	"attr"	INTEGER DEFAULT NULL,
-	FOREIGN KEY("attr") REFERENCES "personne"("id"),
 	FOREIGN KEY("partie_de") REFERENCES "oeuvres"("id"),
-	FOREIGN KEY("auteur") REFERENCES "personne"("id"),
+	FOREIGN KEY("attr") REFERENCES "personnes"("id"),
+	FOREIGN KEY("auteur") REFERENCES "personnes"("id"),
 	PRIMARY KEY("id")
 );
+DROP TABLE IF EXISTS "contient";
 CREATE TABLE IF NOT EXISTS "contient" (
 	"oeuvre"	,
 	"unites_codico"	,
-	FOREIGN KEY("oeuvre") REFERENCES "oeuvres"("id"),
-	FOREIGN KEY("unites_codico") REFERENCES "unites_codico"("id")
+	FOREIGN KEY("unites_codico") REFERENCES "unites_codico"("id"),
+	FOREIGN KEY("oeuvre") REFERENCES "oeuvres"("id")
 );
-CREATE TABLE IF NOT EXISTS "personne" (
-	"id"	INTEGER,
-	"nom"	TEXT,
-	"data_bnf"	INTEGER,
-	PRIMARY KEY("id")
-);
+DROP TABLE IF EXISTS "lieux";
 CREATE TABLE IF NOT EXISTS "lieux" (
 	"id"	INTEGER,
 	"localite"	TEXT NOT NULL,
 	"label"	TEXT,
 	PRIMARY KEY("id")
 );
-CREATE TABLE IF NOT EXISTS "provenances" (
-	"codex"	INTEGER NOT NULL,
-	"lieu"	INTEGER NOT NULL,
-	"cas_particulier"	INTEGER NOT NULL,
-	FOREIGN KEY("lieu") REFERENCES "lieux"("id"),
-	FOREIGN KEY("codex") REFERENCES "codices"("id"),
-	FOREIGN KEY("cas_particulier") REFERENCES "unites_codico"("id")
-);
+DROP TABLE IF EXISTS "production";
 CREATE TABLE IF NOT EXISTS "production" (
 	"codex"	INTEGER NOT NULL,
 	"lieu"	INTEGER NOT NULL,
@@ -70,6 +51,7 @@ CREATE TABLE IF NOT EXISTS "production" (
 	FOREIGN KEY("lieu") REFERENCES "lieux"("id"),
 	FOREIGN KEY("codex") REFERENCES "codices"("id")
 );
+DROP TABLE IF EXISTS "user";
 CREATE TABLE IF NOT EXISTS "user" (
 	"user_id"	INTEGER NOT NULL,
 	"user_nom"	TINYTEXT NOT NULL,
@@ -78,18 +60,41 @@ CREATE TABLE IF NOT EXISTS "user" (
 	"user_password"	VARCHAR(100) NOT NULL,
 	PRIMARY KEY("user_id")
 );
-INSERT INTO "codices" ("id","cote","id_technique","reliure_descript","histoire","lieu_conservation") VALUES (1,'Latin 2989','ark:/12148/cc60815j','Reliure du XVIIIe siècle sur ais de carton en maroquin rouge aux armes royales ; dos décoré par des poinçons, au chiffre royal ; titre au dos en lettres dorées : « Cassianus de monachis ». Contregardes et gardes en parchemin.','Produit dans le Sud-Ouest de la France, le ms. a fait partie de la bibliothèque de l’abbaye de Saint-Pierre de Moissac : il est recensé dans le catalogue de 1678 parmi les livres que Colbert fit venir de Moissac à Paris, mais il est absent du catalogue de Moissac rédigé peu avant cette date (cf. Dufour, « La composition », p. 210). Le volume est donc entré dans la collection de Jean-Baptiste Colbert (1619-1683) (cf. cote ancienne au f. 3r, « Codex Colber. 6156 ») et il entra dans la Bibliothèque du roi en 1732, avec d’autres manuscrits colbertins.',2),
- (2,'Latin 2077','ark:/12148/cc599714','Reliure maroquin rouge aux armes de Colbert.','Provient de l''abbaye de Moissac, cf. Delisle, Cab. des mss ., I, 519.',2),
- (3,'Latin 2388','ark:/12148/cc60220h','Reliure de maroquin rouge, aux armes et au chiffre de Jean-Baptiste Colbert, XVIIe siècle. Estampille de la Bibliothèque royale avant 1735 (modèle Josserand-Bruno, n° 4).','Ce manuscrit n''a vraisemblablement pas été réalisé à Moissac. La présence d''une charte en langue vulgaire (f. 7), datée de 1248, concernant Layrac, dépendance de Moissac, incite à penser que le manuscrit fut écrit et décoré dans les environs d''Agen, peut-être à Layrac même. La copie de cette charte indique dans tous les cas que le manuscrit se trouvait à Layrac au milieu du XIIIe siècle.',2),
- (4,'BPL 1822',NULL,NULL,'Ce manuscrit peut être daté de l''abbatiat de Durand (1048-1072) : son écriture est archaïque mais l''emploi du signe tironien pour ''et'' est fréquent. A la fin du XIe siècle, un copiste combla une lacune du texte (f. 111). Il appartint à Foucault, fut vendu à La Haye en 1721, entra à la bibliothèque universitaire de Leyde en 1905 lors de la vente de la collection de Vroom à Deventer (JD, BSM).',5),
- (5,'Harley 3078',NULL,'Après 1600. Couvrure de cuir aux armes de Nicolas Joseph Foucault.','Une main moderne a inscrit la mention ''Ex Abbatia Moissiacensi''. Après avoir été la propriété de Foucault, il est vendu par le libraire Thomas Ballard à Edward Harley le 20 février 1720/21. La collection Harley, formée par Robert Harley (1661-1724), 1er comte d’Oxford et Mortimer, homme politique, et Edward Harley (1689-1741), 2e comte d’Oxford et Mortimer, collectionneur de livres et mécène, a enregistré l''entrée du codex par la main de leur bibliothécaire, Humfrey Wanley, le 23 février 1720/21 [sic]. Edward Harley légua la bibliothèque à sa veuve, Henrietta Cavendish, née Holles (1694-1755), puis elle fut transmise à leur fille, Margaret Cavendish Bentinck (1715-1785), duchesse de Portland. Les manuscrits ont été vendus par elle en 1753 à la nation britannique pour £10000 en vertu de la loi du Parlement qui a également établi le British Museum ; les manuscrits de Harley forment l’une des collections fondatrices de la British Library.',6),
- (6,'Latin 1656A','ark:/12148/cc59616k','Reliure du XVIIe s. sur ais de carton couverts de maroquin rouge, plats décorés à encadrement doré de triple filet et médaillon central en or aux armes de Colbert ; dos à six nerfs, au chiffre de Colbert et titre en lettres dorées « OPERA / SANCTI / CYPRIANI ». Contre-gardes et gardes en papier, gardes en parchemin. Aux f. 2r et 184v, estampille de la « Bibliotheca regia » identique au modèle Josserand-Bruno n° 5 (Ancien Régime) en usage avant 1735.',NULL,2);
-INSERT INTO "unites_codico" ("id","descript","loc_init","loc_init_v","loc_fin","loc_fin_v","date_pas_avant","date_pas_apres","code_id") VALUES (1,'Parchemin. 154 ff., précédés et suivis d’une garde en parchemin. 180 x 123 mm (justif. 130 x 80 mm). Écriture minuscule caroline d’une main principale.',NULL,NULL,NULL,NULL,975,1000,1),
+DROP TABLE IF EXISTS "personnes";
+CREATE TABLE IF NOT EXISTS "personnes" (
+	"id"	INTEGER,
+	"nom"	TEXT,
+	"data_bnf"	INTEGER,
+	PRIMARY KEY("id")
+);
+DROP TABLE IF EXISTS "codices";
+CREATE TABLE IF NOT EXISTS "codices" (
+	"id"	INTEGER,
+	"cote"	TEXT NOT NULL,
+	"id_technique"	TEXT,
+	"descript_materielle"	TEXT,
+	"histoire"	TEXT,
+	"conservation_id"	INTEGER,
+	FOREIGN KEY("conservation_id") REFERENCES "lieux"("id"),
+	PRIMARY KEY("id")
+);
+DROP TABLE IF EXISTS "provenances";
+CREATE TABLE IF NOT EXISTS "provenances" (
+	"codex"	INTEGER NOT NULL,
+	"lieu"	INTEGER NOT NULL,
+	"origine"	BOOLEAN NOT NULL DEFAULT 0,
+	"remarque"	TEXT,
+	"cas_particulier" INTEGER DEFAULT NULL,
+	FOREIGN KEY("cas_particulier") REFERENCES "unites_codico"("id"),
+	FOREIGN KEY("codex") REFERENCES "codices"("id"),
+	FOREIGN KEY("lieu") REFERENCES "lieux"("id")
+);
+INSERT INTO "unites_codico" ("id","descript","loc_init","loc_init_v","loc_fin","loc_fin_v","date_pas_avant","date_pas_apres","code_id") VALUES (1,'Écriture minuscule caroline d’une main principale.',NULL,NULL,NULL,NULL,975,1000,1),
  (2,NULL,1,1,120,1,981,1020,2),
  (3,'Ecriture régulière de la première moitié du XIe siècle (Dufour, BSM)',121,0,173,0,1001,1050,2),
  (4,NULL,174,0,175,0,1100,1120,2),
  (5,'Les ff. 122 et 123 ont été intercalés.',122,0,123,0,1131,1170,2),
- (6,'Parchemin (certains feuillets sont coupés), 77 ff., 310 x 215 mm (just. 230 x 160 mm). 10 cahiers : 1 cahier de 7 ff. (1-7 ; f. 7 monté), 3 cahiers de 8 ff. (8-31), 1 cahier de 9 ff. (32-40 ; le 10e f. est coupé), 4 cahiers de 8 ff. (41-72), 1 cahier de 5 ff. (73-77 ; montages multiples, présence de talons), précédés et suivis d''une garde de papier et de deux gardes de parchemin. Réclames. Signatures. Piqures.',NULL,NULL,NULL,NULL,1101,1150,3),
+ (6,NULL,NULL,NULL,NULL,NULL,1101,1150,3),
  (7,NULL,NULL,NULL,NULL,NULL,1048,1072,4),
  (8,'Ecrit par une main unique.',NULL,NULL,NULL,NULL,1101,1125,5),
  (9,'Minuscule caroline d’une seule main.',2,1,184,1,1101,1120,6),
@@ -154,7 +159,26 @@ INSERT INTO "contient" ("oeuvre","unites_codico") VALUES (1,1),
  (26,9),
  (27,9),
  (28,9);
-INSERT INTO "personne" ("id","nom","data_bnf") VALUES (1,'Jean Cassien (saint, 0360?-0432?)',12044269),
+INSERT INTO "lieux" ("id","localite","label") VALUES (1,'Moissac','abbaye Saint-Pierre'),
+ (2,'Paris','Bibliothèque nationale de France'),
+ (3,'Layrac','prieuré Saint-Martin'),
+ (4,'Agen',NULL),
+ (5,'Leiden','Universiteitsbibliotheek'),
+ (6,'London','British Library'),
+ (7,'Paris','collection Colbert'),
+ (8,'Paris','collection Foucault'),
+ (9,'London','collection Harley');
+INSERT INTO "production" ("codex","lieu","cas_particulier","incertain","approx") VALUES (1,1,NULL,NULL,NULL),
+ (2,1,NULL,NULL,NULL),
+ (3,3,NULL,1,NULL),
+ (3,4,NULL,1,1),
+ (4,1,NULL,NULL,NULL),
+ (5,1,NULL,NULL,NULL),
+ (6,1,NULL,NULL,NULL);
+INSERT INTO "user" ("user_id","user_nom","user_login","user_email","user_password") VALUES (2,'Monsieur','mau','mau@mau.com','pbkdf2:sha256:260000$6VVfTtBVWLxvkOk8$cc5269b287968e91c0f746c34be966ad5cc2cea015b8d5b25c1782fd72b765a5'),
+ (3,'Barbapapa','bb','barbapapa@gmail.com','pbkdf2:sha256:260000$VdMwwlRdeCOYolak$09a03abb477e2feb8b5986d7fdffed0970c95f4ce425208cb4190512e4941759'),
+ (4,'Piccioncina','piccioncina','piccioncina@gmail.com','pbkdf2:sha256:260000$jEfSctJidxhWD7tp$37a94549ceea5d2277b57110bb4c88b0bb42e1e0026e14725a9d0d2f4f43a315');
+INSERT INTO "personnes" ("id","nom","data_bnf") VALUES (1,'Jean Cassien (saint, 0360?-0432?)',12044269),
  (2,'Ambroise (saint, 0340?-0397)',11888642),
  (3,'Ambroise Autpert (730?-784)',16708997),
  (4,'Augustin (saint, 0354-0430)',11889551),
@@ -170,19 +194,22 @@ INSERT INTO "personne" ("id","nom","data_bnf") VALUES (1,'Jean Cassien (saint, 0
  (14,'Tertullien (0155?-0222?)',11926244),
  (15,'Odon de Meung (10..-10..)',16685825),
  (16,'Macer Floridus (auteur prétendu)',12515583);
-INSERT INTO "lieux" ("id","localite","label") VALUES (1,'Moissac','abbaye Saint-Pierre'),
- (2,'Paris','Bibliothèque nationale de France'),
- (3,'Layrac','prieuré Saint-Martin'),
- (4,'Agen',NULL),
- (5,'Leiden','Universiteitsbibliotheek'),
- (6,'London','British Library');
-INSERT INTO "production" ("codex","lieu","cas_particulier","incertain","approx") VALUES (1,1,NULL,NULL,NULL),
- (2,1,NULL,NULL,NULL),
- (3,3,NULL,1,NULL),
- (3,4,NULL,1,1),
- (4,1,NULL,NULL,NULL),
- (5,1,NULL,NULL,NULL),
- (6,1,NULL,NULL,NULL);
-INSERT INTO "user" ("user_id","user_nom","user_login","user_email","user_password") VALUES (2,'Monsieur','mau','mau@mau.com','pbkdf2:sha256:260000$6VVfTtBVWLxvkOk8$cc5269b287968e91c0f746c34be966ad5cc2cea015b8d5b25c1782fd72b765a5'),
- (3,'Barbapapa','bb','barbapapa@gmail.com','pbkdf2:sha256:260000$VdMwwlRdeCOYolak$09a03abb477e2feb8b5986d7fdffed0970c95f4ce425208cb4190512e4941759');
+INSERT INTO "codices" ("id","cote","id_technique","descript_materielle","histoire","conservation_id") VALUES (1,'Latin 2989','ark:/12148/cc60815j','Reliure du XVIIIe siècle sur ais de carton en maroquin rouge aux armes royales ; dos décoré par des poinçons, au chiffre royal ; titre au dos en lettres dorées : « Cassianus de monachis ». Contregardes et gardes en parchemin. Parchemin. 154 ff., précédés et suivis d’une garde en parchemin. 180 x 123 mm (justif. 130 x 80 mm). ','Produit dans le Sud-Ouest de la France, le ms. a fait partie de la bibliothèque de l’abbaye de Saint-Pierre de Moissac : il est recensé dans le catalogue de 1678 parmi les livres que Colbert fit venir de Moissac à Paris, mais il est absent du catalogue de Moissac rédigé peu avant cette date (cf. Dufour, « La composition », p. 210). Le volume est donc entré dans la collection de Jean-Baptiste Colbert (1619-1683) (cf. cote ancienne au f. 3r, « Codex Colber. 6156 ») et il entra dans la Bibliothèque du roi en 1732, avec d’autres manuscrits colbertins.',2),
+ (2,'Latin 2077','ark:/12148/cc599714','Reliure maroquin rouge aux armes de Colbert.','Provient de l''abbaye de Moissac, cf. Delisle, Cab. des mss ., I, 519.',2),
+ (3,'Latin 2388','ark:/12148/cc60220h','Reliure de maroquin rouge, aux armes et au chiffre de Jean-Baptiste Colbert, XVIIe siècle. Estampille de la Bibliothèque royale avant 1735 (modèle Josserand-Bruno, n° 4). Parchemin (certains feuillets sont coupés), 77 ff., 310 x 215 mm (just. 230 x 160 mm). 10 cahiers : 1 cahier de 7 ff. (1-7 ; f. 7 monté), 3 cahiers de 8 ff. (8-31), 1 cahier de 9 ff. (32-40 ; le 10e f. est coupé), 4 cahiers de 8 ff. (41-72), 1 cahier de 5 ff. (73-77 ; montages multiples, présence de talons), précédés et suivis d''une garde de papier et de deux gardes de parchemin. Réclames. Signatures. Piqures.','Ce manuscrit n''a vraisemblablement pas été réalisé à Moissac. La présence d''une charte en langue vulgaire (f. 7), datée de 1248, concernant Layrac, dépendance de Moissac, incite à penser que le manuscrit fut écrit et décoré dans les environs d''Agen, peut-être à Layrac même. La copie de cette charte indique dans tous les cas que le manuscrit se trouvait à Layrac au milieu du XIIIe siècle.',2),
+ (4,'BPL 1822',NULL,NULL,'Ce manuscrit peut être daté de l''abbatiat de Durand (1048-1072) : son écriture est archaïque mais l''emploi du signe tironien pour ''et'' est fréquent. A la fin du XIe siècle, un copiste combla une lacune du texte (f. 111). Il appartint à Foucault, fut vendu à La Haye en 1721, entra à la bibliothèque universitaire de Leyde en 1905 lors de la vente de la collection de Vroom à Deventer (JD, BSM).',5),
+ (5,'Harley 3078',NULL,'Après 1600. Couvrure de cuir aux armes de Nicolas Joseph Foucault.','Une main moderne a inscrit la mention ''Ex Abbatia Moissiacensi''. Après avoir été la propriété de Foucault, il est vendu par le libraire Thomas Ballard à Edward Harley le 20 février 1720/21. La collection Harley, formée par Robert Harley (1661-1724), 1er comte d’Oxford et Mortimer, homme politique, et Edward Harley (1689-1741), 2e comte d’Oxford et Mortimer, collectionneur de livres et mécène, a enregistré l''entrée du codex par la main de leur bibliothécaire, Humfrey Wanley, le 23 février 1720/21 [sic]. Edward Harley légua la bibliothèque à sa veuve, Henrietta Cavendish, née Holles (1694-1755), puis elle fut transmise à leur fille, Margaret Cavendish Bentinck (1715-1785), duchesse de Portland. Les manuscrits ont été vendus par elle en 1753 à la nation britannique pour £10000 en vertu de la loi du Parlement qui a également établi le British Museum ; les manuscrits de Harley forment l’une des collections fondatrices de la British Library.',6),
+ (6,'Latin 1656A','ark:/12148/cc59616k','Reliure du XVIIe s. sur ais de carton couverts de maroquin rouge, plats décorés à encadrement doré de triple filet et médaillon central en or aux armes de Colbert ; dos à six nerfs, au chiffre de Colbert et titre en lettres dorées « OPERA / SANCTI / CYPRIANI ». Contre-gardes et gardes en papier, gardes en parchemin. Aux f. 2r et 184v, estampille de la « Bibliotheca regia » identique au modèle Josserand-Bruno n° 5 (Ancien Régime) en usage avant 1735.',NULL,2);
+INSERT INTO "provenances" ("codex","lieu","origine","remarque") VALUES (1,1,1,NULL),
+ (1,7,0,NULL),
+ (3,7,0,NULL),
+ (2,1,1,NULL),
+ (3,3,1,'?'),
+ (3,4,1,'région de'),
+ (4,1,1,NULL),
+ (4,8,0,NULL),
+ (5,1,1,NULL),
+ (5,8,0,NULL),
+ (6,1,1,NULL),
+ (6,9,0,NULL);
 COMMIT;
--- app/modeles/classes.py
+++ app/modeles/classes.py
@@ -9,22 +9,15 @@ class Codices(db.Model):
     id_technique = db.Column(db.String(19))
     descript_materielle = db.Column(db.Text)
     histoire = db.Column(db.Text)
-    conservation_id = db.Column(db.Integer, db.ForeignKey('lieux.id'))
+    conservation_id = db.Column(db.Integer, db.ForeignKey("lieux.id"))
     conservation = db.relationship("Lieux", back_populates="codex")
     unites_codico = db.relationship("Unites_codico", back_populates="codex")
 
 
-# Ancienne classe
-"""
-class Contient(db.Model):
-    rowid = db.Column(db.Integer, primary_key=True)
-    oeuvre = db.Column(db.Integer, nullable=False)
-    unites_codico = db.Column(db.Integer, nullable=False)
-"""
-# Table de relation
-contient = Table('contient', db.metadata,
-                 Column('oeuvre', ForeignKey('oeuvres.id')),
-                 Column('unites_codico', ForeignKey('unites_codico.id'))
+# Table de relation "contient"
+contient = Table("contient", db.metadata,
+                 Column("oeuvre", ForeignKey("oeuvres.id")),
+                 Column("unites_codico", ForeignKey("unites_codico.id"))
                  )
 
 
@@ -32,16 +25,16 @@ class Unites_codico(db.Model):
     id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
     # Description physique
     descript = db.Column(db.Text)
-    # Localisation d'une unité dans un codex (f. n-f. m)
+    # Localisation d"une unité dans un codex (f. n-f. m)
     loc_init = db.Column(db.Integer, default=None)
     loc_init_v = db.Column(db.Boolean, default=None)
     loc_fin = db.Column(db.Integer, default=None)
     loc_fin_v = db.Column(db.Boolean, default=None)
     date_pas_avant = db.Column(db.Integer, nullable=False)
     date_pas_apres = db.Column(db.Integer, nullable=False)
-    code_id = db.Column(db.Integer, db.ForeignKey('codices.id'))
-    codex = db.relationship('Codices', back_populates="unites_codico")
-    contenu = db.relationship('Oeuvres', secondary=contient, backref='Unites_codico')
+    code_id = db.Column(db.Integer, db.ForeignKey("codices.id"))
+    codex = db.relationship("Codices", back_populates="unites_codico")
+    contenu = db.relationship("Oeuvres", secondary=contient, backref="Unites_codico")
 
 
 class Oeuvres(db.Model):
@@ -49,19 +42,19 @@ class Oeuvres(db.Model):
     titre = db.Column(db.Text, nullable=False)
     data_bnf = db.Column(db.Integer, nullable=True)
     partie_de = db.Column(db.Boolean, nullable=True)
-    unites_codico = db.relationship('Unites_codico', secondary=contient, backref='Oeuvres')
-    auteur = db.Column(db.Integer, db.ForeignKey('personnes.id'))
-    lien_auteur = db.relationship("Personnes", back_populates='oeuvres_aut', foreign_keys=auteur)
-    attr = db.Column(db.Integer, db.ForeignKey('personnes.id'))
-    lien_attr = db.relationship("Personnes", back_populates='oeuvres_attr', foreign_keys=attr)
+    unites_codico = db.relationship("Unites_codico", secondary=contient, backref="Oeuvres")
+    auteur = db.Column(db.Integer, db.ForeignKey("personnes.id"))
+    lien_auteur = db.relationship("Personnes", back_populates="oeuvres_aut", foreign_keys=auteur)
+    attr = db.Column(db.Integer, db.ForeignKey("personnes.id"))
+    lien_attr = db.relationship("Personnes", back_populates="oeuvres_attr", foreign_keys=attr)
 
 
 class Personnes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nom = db.Column(db.Text, nullable=False)
     data_bnf = db.Column(db.Integer, nullable=True)
-    oeuvres_aut = db.relationship("Oeuvres", back_populates='lien_auteur', foreign_keys=Oeuvres.auteur)
-    oeuvres_attr = db.relationship("Oeuvres", back_populates='lien_attr', foreign_keys=Oeuvres.attr)
+    oeuvres_aut = db.relationship("Oeuvres", back_populates="lien_auteur", foreign_keys=Oeuvres.auteur)
+    oeuvres_attr = db.relationship("Oeuvres", back_populates="lien_attr", foreign_keys=Oeuvres.attr)
 
 
 class Lieux(db.Model):
@@ -70,4 +63,11 @@ class Lieux(db.Model):
     label = db.Column(db.String(30))
     codex = db.relationship("Codices", back_populates="conservation")
 
-
+# La table de relations "provenances" lie les classes "Lieux" et "Codices"
+provenances = Table("provenances")
+"""
+contient = Table("contient", db.metadata,
+                 Column("oeuvre", ForeignKey("oeuvres.id")),
+                 Column("unites_codico", ForeignKey("unites_codico.id"))
+                 )
+                 """
