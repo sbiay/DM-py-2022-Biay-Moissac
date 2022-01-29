2e8a42eca2f79547f213a2b24c3b3fa41ecbdf85
Sebastien Biay <sbiayha@outlook.fr>
2022-01-29 20:43:24 +0100

    Page codices reconstituée !

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from ..appliMoissac import app, login
 from ..modeles.classes import Codices, Lieux, Unites_codico, Oeuvres, Personnes, Provenances
 from ..modeles.utilisateurs import User
-from ..modeles.jointures import labelCodex, toutes_oeuvres, tous_auteurs
+from ..modeles.jointures import labelCodex, toutes_oeuvres, tous_auteurs, codexJson
 from ..comutTest import test
 
 
@@ -90,66 +90,19 @@ def inscription():
 
 @app.route("/pages/codices/<int:num>")
 def notice_codex(num):
+    # Test d'existence de l'identifiant cherché
     codex = Codices.query.get_or_404(num)
     
-    # Pour le lieu de conservation et la cote du codex
-    label = labelCodex(num)[num]
-    
-    # Eléments descriptifs de chaque unité codicologique
-    descUCs = []
-    for UC in listUC_enfants:
-        descUC = {}
-        descUC["description"] = UC.descript
-        
-        
-        descUC["date"] = f"entre {UC.date_pas_avant} et {UC.date_pas_apres}"
-        
-        # Il faut à présent boucler sur les contenus de chaque UC
-        requ_contenu = Contient.query.filter(Contient.unites_codico == UC.id).all()
-        # requ_contenu est une liste de mapping possédant comme propriétés .oeuvre et .unites_codico
-        # et ce d'après la table de relation "contient"
-        
-        # On peut donc initier une liste dont les valeurs seront les résultats des requêtes sur
-        # la classe Oeuvres en fonction du mapping assigné précédemment
-        contenu = []
-        # En bouclant sur les items de cette requête, je requête la classe Oeuvres pour obtenir
-        # les données relatives à chaque oeuvre de l'unité codicologique présente
-        for items in requ_contenu:
-            auteur = Oeuvres.query.get(items.oeuvre).auteur
-            titre = Oeuvres.query.get(items.oeuvre).titre
-            if auteur:
-                # Le nom sous la forme d'autorité (avec fonction et dates entre parenthèses)
-                nom = Personnes.query.get(auteur).nom
-                
-                # On retient pour la page le nom sans les parenthèses, sauf si elles contiennent un titre (pape,
-                # saint, etc)
-                # Si le premier caractère après la parenthèse n'est pas un chiffre, c'est un titre (on dira "role"
-                # pour éviter les confusions avec les titres d'oeuvre)
-                # retenir :
-                if nom.split("(")[1][0] not in "0123456789":
-                    role = nom.split("(")[1].split(",")[0]
-                    # Le nom sans les dates, suivi du role
-                    nom = f"{nom.split('(')[0][:-1]} ({role})"
-                else:
-                    nom = f"{nom.split('(')[0][:-1]}"
-            else:
-                nom = "Anonyme"
-            contenu.append((nom, titre))
-        
-        descUC["contenu"] = contenu
-        
-        # Après avoir bouclé sur les items du contenu, on ajoute le dictionnaire à la liste descUCs
-        descUCs.append(descUC)
-    
-    # A la fin de ma boucle sur les unités codicologiques, la liste descUCs contient les données
-    # relatives à chacune.
+    # Réassignation de la variable codex par l'objet Json retourné par la fonction codexJson()
+    codex = json.loads(codexJson(num))
+    print(type(codex))
     
     if not test:
         return render_template("pages/codices.html",
-                               titre=label,
-                               materielle=codex.descript_materielle,
-                               histoire=codex.histoire,
-                               descUCs=descUCs)
+                               titre=codex["label"],
+                               materielle=codex["description_materielle"],
+                               histoire=codex["histoire"],
+                               descUCs=codex["contenu"])
 
 
 @app.route("/recherche")
